72a94fa94865d15df71beab2f16267c08e456f9d
juliakobets <jjjnek@gmail.com>
2026-05-21 09:13:55 +0200

    Initial commit

---
+++ src/main/java/newproject/msaccountreservation/MsAccountReservationApplication.java
@@ -0,0 +1,13 @@
+package newproject.msaccountreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountReservationApplication.class, args);
+    }
+
+}
