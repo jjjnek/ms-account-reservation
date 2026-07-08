076eee23209706fc3cb2a96eb8eb62ff94deb245
juliakobets <jjjnek@gmail.com>
2026-07-08 15:38:54 +0200

    UPGJK-5. Настроить логирование

--- src/main/java/newproject/msaccountreservation/MsAccountReservationApplication.java
+++ src/main/java/newproject/msaccountreservation/MsAccountReservationApplication.java
@@ -1,13 +1,22 @@
 package newproject.msaccountreservation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsAccountReservationApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(MsAccountReservationApplication.class);
+
     public static void main(String[] args) {
+
+        log.info("Запуск приложения...");
+
         SpringApplication.run(MsAccountReservationApplication.class, args);
+
+        log.info("Приложение успешно запущено");
     }
 
 }
